b532514aa8fd990def28e7151f37e25fd457d1df
AmirHossein Jahankheir Amlashi <amir9877h@gmail.com>
2024-05-10 11:54:29 +0330

    screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 
@@ -63,6 +64,101 @@ class _MainScreenState extends State<MainScreen> {
       body: Stack(
         children: [
           Positioned.fill(child: VideoPlayer(_controller)),
+          Positioned.fill(
+              child: Container(
+            padding: const EdgeInsets.fromLTRB(16, 48, 16, 20),
+            decoration: BoxDecoration(
+              color: Colors.black.withOpacity(0.2),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Row(
+                  children: [
+                    Icon(Icons.person_pin_rounded),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Name',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        Text(
+                          'id',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Video title',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    const Text(
+                      'Video subtitle',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    VideoProgressIndicator(
+                      _controller,
+                      allowScrubbing: true,
+                      colors:
+                          const VideoProgressColors(playedColor: Colors.white),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'time',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        Text(
+                          'time',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        IconButton(
+                            onPressed: () {},
+                            iconSize: 56,
+                            icon: const Icon(
+                              CupertinoIcons.backward_end_alt,
+                              color: Colors.white,
+                            )),
+                        IconButton(
+                            onPressed: () {
+                              // setState(() {
+                              //   _controller.value.ser = !_controller.value.isPlaying;
+                              // });
+                            },
+                            iconSize: 56,
+                            icon: Icon(
+                              _controller.value.isPlaying
+                                  ? CupertinoIcons.pause_circle
+                                  : CupertinoIcons.play_circle,
+                              color: Colors.white,
+                            )),
+                        IconButton(
+                            onPressed: () {},
+                            iconSize: 56,
+                            icon: const Icon(
+                              CupertinoIcons.forward_end_alt,
+                              color: Colors.white,
+                            )),
+                      ],
+                    ),
+                  ],
+                )
+              ],
+            ),
+          )),
         ],
       ),
     );
